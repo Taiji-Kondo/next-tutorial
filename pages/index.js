5df43ea88127be14a95b6064cff0081ee1b5faae
Taiji Kondo <kondo@sailboat.co.jp>
2021-09-01 18:32:07 +0900

    ADD: Use the next-head

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Next Tutorial</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
