a8986a25c878da967efc9866648f58100d0b9bd6
Taiji Kondo <kondo@sailboat.co.jp>
2021-09-03 16:41:45 +0900

    ADD: Add null pattern to Date component

--- components/Date.tsx
+++ components/Date.tsx
@@ -2,10 +2,12 @@ import { parseISO, format } from 'date-fns'
 import {VFC} from "react";
 
 type DatePropsType = {
-  dateString: string
+  dateString?: string
 }
 
 export const Date: VFC<DatePropsType> = ({ dateString }) => {
+  if (!dateString) return <time>-</time>
+
   const date = parseISO(dateString)
   return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>
 }
